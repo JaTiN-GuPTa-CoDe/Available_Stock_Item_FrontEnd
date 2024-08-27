0ac8b78b15d59b594170735005ea6d86dc9c819f
Jatin Gupta <144834913+codewith-jatin@users.noreply.github.com>
2024-08-27 23:50:07 +0530

    update code

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
+import { Link } from 'react-router-dom';
+import '../css/Login.css'
 
 function Login() {
   return (
     <>
      <NavBar />
-      Login
+     <div class="box">
+            <div class="form">
+                <h2>Login</h2>
+                <div class="inputBox">
+                    <input type="text" required="required"/>
+                    <span>Username</span>
+                    <i></i>
+                </div>
+                <div class="inputBox">
+                    <input type="password" required="required"/>
+                    <span>Password</span>
+                    <i></i>
+                </div>
+                <div class="links">
+                    {/* <a href="https://akhs1.com/">Forgot password?</a> */}
+                    <Link to="/signup">Sign Up</Link>
+                </div>
+                <a href="https://akhs1.com/"><input type="submit" value="Login"/></a>
+            </div>
+        </div>
       <Footer/>
     </>
   )
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+import '../css/Signup.css';
 
 function Signup() {
   return (
     <>
-     <NavBar />
-      Signup
-      <Footer/>
+      <NavBar />
+      <div className="signup-box">
+        <div className="signup-form">
+          <h2>Sign Up</h2>
+          <div className="signup-inputBox">
+            <input type="text" required="required" />
+            <span>Username</span>
+            <i></i>
+          </div>
+          <div className="signup-inputBox">
+            <input type="email" required="required" />
+            <span>Email</span>
+            <i></i>
+          </div>
+          <div className="signup-inputBox">
+            <input type="password" required="required" />
+            <span>Password</span>
+            <i></i>
+          </div>
+          <div className="signup-links">
+            <Link to="/login">Already have an account? Login</Link>
+          </div>
+          <input type="submit" value="Sign Up" />
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
