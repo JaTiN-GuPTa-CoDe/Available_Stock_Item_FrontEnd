7874dc4d08aa04f391c7432c6c20709b5c763789
Jatin Gupta <144834913+codewith-jatin@users.noreply.github.com>
2024-08-28 00:38:48 +0530

    update code

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,37 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 function Contact() {
     return (
         <>
             <NavBar />
-            Contact
-            <Footer/>
+            <div className="center-wrapper">
+                <div className="box">
+                    <div className="form">
+                        <h2>Contact Us</h2>
+                        <div className="inputBox nameInputBox">
+                            <input type="text" required="required" />
+                            <span>Name</span>
+                            <i></i>
+                        </div>
+                        <div className="inputBox phoneInputBox">
+                            <input type="number" required="required" />
+                            <span>Phone No</span>
+                            <i></i>
+                        </div>
+                        <div className="inputBox messageInputBox">
+                            <input type="text" required="required" />
+                            <span>Message</span>
+                            <i></i>
+                        </div>
+                        <input type="submit" className="submitBtn" value="Submit" />
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </>
-    )
+    );
 }
 
-export default Contact
+export default Contact;
